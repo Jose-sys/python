cfce8c08a3da261f360c03ca123bf66216b88d27
Diego <diego2737@gmail.com>
2013-05-09 19:24:26 -0300

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+"calcular numero de fibonacci de forma optima"
+
+def fib(i):
+    lista = [1,1]
+    while len(lista) <= i+1:
+        lista.append(lista[-1] + lista[-2])
+    return lista[-1]
+
+
+fib(5000000000)
